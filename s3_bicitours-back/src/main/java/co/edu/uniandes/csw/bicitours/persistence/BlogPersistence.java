3700ec5c389813e771eb404c9223af834133c594
Oscar Julian C. G. <oj.castaneda@uniandes.edu.co>
2019-08-22 18:59:44 -0500

    Pruebas persistencia clase Blog

---
+++ s3_bicitours-back/src/main/java/co/edu/uniandes/csw/bicitours/persistence/BlogPersistence.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.bicitours.persistence;
+
+import co.edu.uniandes.csw.bicitours.entities.BlogEntity;
+import java.util.logging.Logger;
+import java.util.List;
+import java.util.logging.Level;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ * Clase que maneja la persistencia para Blog. Se conecta a través del Entity
+ * Manager de javax.persistance con la base de datos SQL.
+ * 
+ * @author Oscar Julian Castañeda G.
+ */
+@Stateless
+public class BlogPersistence {
+    @PersistenceContext(unitName="bicitoursPU")
+    protected EntityManager em;
+    
+    private static final Logger LOGGER= Logger.getLogger(BlogPersistence.class.getName());
+     /**
+     * Método para persisitir la entidad en la base de datos.
+     *
+     * @param blogEntity objeto blog que se creará en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
+    public BlogEntity create(BlogEntity blogEntity)
+    {
+        LOGGER.log(Level.INFO, "Creando nuevo blog");
+        em.persist(blogEntity);
+        LOGGER.log(Level.INFO, "Blog creado");
+        return blogEntity;
+    }
+    /**
+     * Busca si hay algun blog con el id que se envía de argumento
+     *
+     * @param blogId: id correspondiente al blog buscado.
+     * @return un blog.
+     */
+    public BlogEntity find(long blogId)
+    {
+        LOGGER.log(Level.INFO, "Consultando el blog con id={0}", blogId);
+        return em.find(BlogEntity.class, blogId);
+    }
+        /**
+     * Devuelve todos los blogs de la base de datos.
+     *
+     * @return una lista con todos los blogs que encuentre en la base de datos,
+     * "select u from blogEntity u" es como un "select * from BlogEntity;" -
+     * "SELECT * FROM table_name" en SQL.
+     */
+    public List<BlogEntity> findAll()
+    {
+        LOGGER.log(Level.INFO, "Consultando todos los blogs");
+        TypedQuery<BlogEntity> query=em.createQuery("select u from BlogEntity u", BlogEntity.class);
+        return query.getResultList();
+    }
+     /**
+     * Actualiza un blog.
+     *
+     * @param blogEntity: el libro que viene con los nuevos cambios. Por ejemplo
+     * el texto pudo cambiar. En ese caso, se haria uso del método update.
+     * @return un blog con los cambios aplicados.
+     */
+    public BlogEntity update(BlogEntity blogEntity) {
+        LOGGER.log(Level.INFO, "Actualizando el blog con id={0}", blogEntity.getId());
+        return em.merge(blogEntity);
+    }
+
+    /**
+     *
+     * Borra un blog de la base de datos recibiendo como argumento el id del
+     * blog
+     *
+     * @param blogId: id correspondiente al blog a borrar.
+     */
+    public void delete(Long blogId) {
+        LOGGER.log(Level.INFO, "Borrando el blog con id={0}", blogId);
+        BlogEntity blogEntity = em.find(BlogEntity.class, blogId);
+        em.remove(blogEntity);
+    }
+}
---
+++ s3_bicitours-back/src/test/java/co/edu/uniandes/csw/bicitours/test/persistence/BlogPersistenceTest.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.bicitours.test.persistence;
+import co.edu.uniandes.csw.bicitours.entities.BlogEntity;
+import co.edu.uniandes.csw.bicitours.persistence.BlogPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import junit.framework.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ * Pruebas de persistencia de Blogs
+ * 
+ * @author Oscar Julian Castañeda G.
+ */
+@RunWith(Arquillian.class)
+public class BlogPersistenceTest {
+    @Inject
+    private BlogPersistence bp;
+    @PersistenceContext
+    private EntityManager em;
+    @Inject 
+    UserTransaction utx;
+    private List<BlogEntity> data = new ArrayList<BlogEntity>();
+    /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment()
+    {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(BlogEntity.class)
+                .addClass(BlogPersistence.class)
+                .addAsManifestResource("META-INF/persistence.xml","persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml","beans.xml");
+    }
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from BlogEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            BlogEntity entity = factory.manufacturePojo(BlogEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+     /**
+     * Prueba para crear un Blog.
+     */
+    @Test
+    public void createTestBlog()
+    {
+        PodamFactory factory= new PodamFactoryImpl();
+        BlogEntity newEntity= factory.manufacturePojo(BlogEntity.class);
+        BlogEntity result=bp.create(newEntity);
+        Assert.assertNotNull(result);
+        BlogEntity entity = em.find(BlogEntity.class, result.getId());
+        Assert.assertEquals(newEntity.getTexto(),entity.getTexto());
+        Assert.assertEquals(newEntity.getRutaImagen(),entity.getRutaImagen());
+        Assert.assertEquals(newEntity.getRutaVideo(),entity.getRutaVideo());
+        Assert.assertEquals(newEntity.getCalificacionPromedio(),entity.getCalificacionPromedio());
+    }
+    /**
+     * Prueba para consultar la lista de Blogs.
+     */
+    @Test
+    public void getBlogsTest() {
+        List<BlogEntity> list = bp.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (BlogEntity ent : list) {
+            boolean found = false;
+            for (BlogEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+
+    /**
+     * Prueba para consultar un Blog.
+     */
+    @Test
+    public void getBlogTest() {
+        BlogEntity entity = data.get(0);
+        BlogEntity newEntity = bp.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(newEntity.getTexto(),entity.getTexto());
+        Assert.assertEquals(newEntity.getRutaImagen(),entity.getRutaImagen());
+        Assert.assertEquals(newEntity.getRutaVideo(),entity.getRutaVideo());
+        Assert.assertEquals(newEntity.getCalificacionPromedio(),entity.getCalificacionPromedio());
+    }
+
+    /**
+     * Prueba para eliminar un Blog.
+     */
+    @Test
+    public void deleteBlogTest() {
+        BlogEntity entity = data.get(0);
+        bp.delete(entity.getId());
+        BlogEntity deleted = em.find(BlogEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
+    /**
+     * Prueba para actualizar un Blog.
+     */
+    @Test
+    public void updateBlogTest() {
+        BlogEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        BlogEntity newEntity = factory.manufacturePojo(BlogEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        bp.update(newEntity);
+
+        BlogEntity resp = em.find(BlogEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getTexto(),resp.getTexto());
+        Assert.assertEquals(newEntity.getRutaImagen(),resp.getRutaImagen());
+        Assert.assertEquals(newEntity.getRutaVideo(),resp.getRutaVideo());
+        Assert.assertEquals(newEntity.getCalificacionPromedio(),resp.getCalificacionPromedio());
+    }
+
+}
