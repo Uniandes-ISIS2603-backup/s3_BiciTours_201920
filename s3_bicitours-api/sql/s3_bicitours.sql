7d08bc95f6a436a3ce0fac4e4b831068281f8dc1
Maria Clara Noguera Echeverri <mc.noguera@uniandes.edu.co>
2019-10-10 09:56:43 -0500

    sql

--- s3_bicitours-api/sql/s3_bicitours.sql
+++ s3_bicitours-api/sql/s3_bicitours.sql
@@ -9,3 +9,7 @@ update BlogEntity set siguiente_id = 1 where id = 0;
 update BlogEntity set siguiente_id = 2 where id = 1;
 update BlogEntity set anterior_id = 2 where id = 6;
 update BlogEntity set anterior_id = 6 where id = 0;
+
+ insert into SEGUROENTITY (ID,CARACTERISTICAS,CONDICIONES,TIPO) VALUES (0,'Seguro contra robo de bicicletas','El robo de debe reportar 24 horas despues de sucedido para hacer valido el seguro', 'Antirobo');
+ insert into SEGUROENTITY (ID,CARACTERISTICAS,CONDICIONES,TIPO) VALUES (1,'Seguro para fallecimiento','Se hace efectivo si es cliente fallece en un accidente durante el tour', 'Seguro de vida');
+ insert into SEGUROENTITY (ID,CARACTERISTICAS,CONDICIONES,TIPO) VALUES (2,'Seguro para lesiones','Se cubren todos los gastos medicos que requiera el participante tras sufrir una lesion en el tour', 'Seguro de salud');
