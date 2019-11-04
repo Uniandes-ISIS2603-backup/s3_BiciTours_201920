217909123176785d37330f23c75ec906b14d0730
Oscar Julian Castada G. <oj.castaneda@uniandes.com>
2019-11-03 20:31:47 -0500

    Intento correccion BlogIT 2

--- s3_bicitours-api/src/test/java/co/edu/uniandes/csw/bicitours/tests/postman/BlogIT.java
+++ s3_bicitours-api/src/test/java/co/edu/uniandes/csw/bicitours/tests/postman/BlogIT.java
@@ -23,16 +23,16 @@ import org.junit.runner.RunWith;
 
 /**
  *
- * @author user
+ * @author Oscar Julian Castañeda G.
  */
 @RunWith(Arquillian.class)
 public class BlogIT {
 
-    private static final String COLLECTION = "blog.postman_collection";
+    private static final String COLLECTION = "BlogResourceTest.postman_collection";
 
     @Deployment(testable = true)
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, "frontstepbystep-api.war")//War del modulo api
+        return ShrinkWrap.create(WebArchive.class, "s3_bicitours-api-1.0.war")//War del modulo api
                 // Se agrega las dependencias
                 .addAsLibraries(Maven.resolver().loadPomFromFile("pom.xml")
                         .importRuntimeDependencies().resolve()
