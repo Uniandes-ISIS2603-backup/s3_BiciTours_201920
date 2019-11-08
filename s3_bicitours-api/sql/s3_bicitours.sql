441773dc0fb277e83b2b0233b12a605122d0ee24
Oscar Julian Castada G. <oj.castaneda@uniandes.com>
2019-11-08 18:43:49 -0500

    Cambios en el sql y colecciones

--- s3_bicitours-api/sql/s3_bicitours.sql
+++ s3_bicitours-api/sql/s3_bicitours.sql
@@ -5,11 +5,30 @@ delete from SeguroEntity;
 delete from ComentarioEntity;
 delete from TourEntity;
 
-insert into BlogEntity (id, rutaImagen, rutaVideo, texto, titulo, calificacionPromedio) values (0, 'D:\', 'G:\', 'A', 'E', 10);
-insert into BlogEntity (id, rutaImagen, rutaVideo, texto, titulo, calificacionPromedio) values (1, 'D:\', 'G:\', 'B', 'F', 11);
-insert into BlogEntity (id, rutaImagen, rutaVideo, texto, titulo, calificacionPromedio) values (2, 'C:\', 'F:\', 'C', 'G', 12);
-insert into BlogEntity (id, rutaImagen, rutaVideo, texto, titulo, calificacionPromedio) values (6, 'C:\', 'F:\', 'D', 'H', 16);
-insert into BlogEntity (id, rutaImagen, rutaVideo, texto, titulo, calificacionPromedio) values (79, 'C:\', 'F:\', 'D', 'H', 16);
+insert into BlogEntity (id, titulo, calificacionPromedio, texto, rutaImagen) 
+values (1,
+'My name is Yoshikage Kira',
+10.0,'My name is Yoshikage Kira. I''m 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest...',/**
+'My name is Yoshikage Kira. I''m 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don''t smoke, but I occasionally drink.I''m in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up.I''m trying to explain that I''m a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn''t lose to anyone.',
+*/'https://i.kym-cdn.com/entries/icons/original/000/029/929/Kira_decides_to_tail_Shigechi.png');
+insert into BlogEntity (id, titulo, calificacionPromedio, texto, rutaVideo ) 
+values (2,
+'the tragedy of Darth Plagueis The Wise',
+66.0,'Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It''s not a story the Jedi would tell you. It''s a Sith legend...',/**
+'Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It''s not a story the Jedi would tell you. It''s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.',
+*/'https://youtu.be/05dT34hGRdg' );
+insert into BlogEntity (id, titulo, calificacionPromedio, texto, rutaVideo) 
+values (3,
+'BiciGo',
+9.5,
+'Felicidad y salud al alcance de tu bici',
+'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+insert into BlogEntity (id, titulo, calificacionPromedio, texto, rutaImagen) 
+values (4,
+'Azer isn''t so great?',
+727.0,'Azer isn''t so great? Are you kidding me? When was the last time you saw a player with such aim ability and movement with a tablet?',/**
+'Azer isn''t so great? Are you kidding me? When was the last time you saw a player with such aim ability and movement with a tablet? Alex puts the game in another level, and we will be blessed if we ever see a player with his skill and passion for the game again. Cookiezi breaks records. Rafis breaks records. Azer breaks the rules. You can keep your statistics. I prefer the magic.',
+*/'https://upload.wikimedia.org/wikipedia/commons/d/d3/Osu%21Logo_%282015%29.png');
 
 insert into TourEntity (id, nombre, lugar, descripcion, duracion, costo, terminado) values(0, 'tour', 'Neiva', 'Traer bici', 3, 3, 0);
 insert into TourEntity (id, nombre, lugar, descripcion, duracion, costo, terminado) values(1, 'tours', 'Neivas', 'Traer bicis', 4, 4, 0);
@@ -28,6 +47,3 @@ insert into UsuarioEntity (id, nombre, codigo, correo, pago, esAdmin, deuda) val
 
 insert into ComentarioEntity (id, texto, calificacion) values (0, 'Súper', 5);
 insert into ComentarioEntity (id, texto, calificacion) values (1, 'Nada mal', 4);
-
-update ComentarioEntity set blog_id = 0, usuario_id = 100 where id = 0;
-update ComentarioEntity set blog_id = 0, usuario_id = 200 where id = 1;
